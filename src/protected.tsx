abb8ac9ff80f7b3e513bcb23dbeab669fc1c30f5
Annukul Thakran <annukulthakran@Annukuls-MacBook-Air.local>
2023-03-19 15:05:34 +0530

    Timeout added

--- src/protected.tsx
+++ src/protected.tsx
@@ -1,19 +1,25 @@
 "use client"
-import React, { ReactNode, useEffect } from "react"
+import React, { ReactNode, useEffect, useState } from "react"
 import { useRouter } from "next/navigation"
 
 export const Protected = ({ children }: { children: ReactNode }) => {
-  const isLoggedIn = true
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
   const router = useRouter()
+
+  setTimeout(() => {
+    setIsLoggedIn(true)
+  }, 500)
   
   useEffect(() => {
     if (!isLoggedIn) {
+        setIsLoggedIn(false)
         router.push("/login")
     } else {
+        setIsLoggedIn(true)
         router.push("/")
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isLoggedIn])
 
-  return <React.Fragment>{children}</React.Fragment>
+  return <React.Fragment>{isLoggedIn ? children : null}</React.Fragment>
 }
